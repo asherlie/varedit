54c4547e0b4119f105f604e22682db3ad8fe6d46
asherlie <lieber.asher@gmail.com>
2018-02-20 17:44:19 -0500

    updated readme, updated vmem_access.h to be more consistent with vmem_access.cpp

--- vmem_access.h
+++ vmem_access.h
@@ -15,7 +15,7 @@ int read_single_val_from_pid_mem(pid_t pid, int bytes, void* vm);
 std::string read_str_from_mem_block_slow(pid_t pid, void* mb_start, void* mb_end);
 bool write_int_to_pid_mem(pid_t pid, void* vm, int value);
 bool write_str_to_pid_mem(pid_t pid, void* vm, std::string str);
-void populate_mem_map(mem_map &mmap, pid_t pid, int d_rgn, bool additiona, bool integers);
+void populate_mem_map(mem_map &mmap, pid_t pid, int d_rgn, bool use_additional_rgns, bool integers);
 void update_mem_map(mem_map &mem, bool integers);
 void narrow_mem_map_int(mem_map &mem, int match);
 void narrow_mem_map_str(mem_map &mem, std::string match, bool exact);
