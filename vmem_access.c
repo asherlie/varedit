808a0bd714f31f73ee04936e1a5af9aea8577f09
asherlie <lieber.asher@gmail.com>
2018-06-22 00:56:05 -0400

    fixed a bug that caused incorrect narrow_mem_map_str results when exact_e mode was used and the search string occured more than once in a mem_map string

--- vmem_access.c
+++ vmem_access.c
@@ -340,12 +340,24 @@ void narrow_mem_map_int(struct mem_map* mem, int match){
       }
 }
 
+// this function finds the last match of needle in haystack, useful for exact_e in narrow_mem_map_str
+// TODO: is there a better way to do this?
+char* last_m(char* haystack, const char* needle){
+      char* c = haystack;
+      char* p;
+      while((p = strstr(c, needle))){
+            ++p;
+            c = p;
+      }
+      return c-1;
+}
+
 void narrow_mem_map_str(struct mem_map* mem, const char* match, bool exact_s, bool exact_e){
       unsigned int initial = mem->size;
       int mlen = strlen(match);
       for(unsigned int i = 0; i < mem->size; ++i){
             char* s = strstr(mem->cp_mmap[i].value, match);
-            if(!s || (exact_s && s != mem->cp_mmap[i].value) || (exact_e && s[mlen] != '\0')){
+            if(!s || (exact_s && s != mem->cp_mmap[i].value) || (exact_e && last_m(mem->cp_mmap[i].value, match)[mlen] != '\0')){
                   if(!mem->blk->in_place)free(mem->cp_mmap[i].value);
                   --mem->size;
                   if(mem->size == 0)break;
