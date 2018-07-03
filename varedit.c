003cf176b8ced785b83d6f5a3f75e4fc8edecbb3
asherlie <lieber.asher@gmail.com>
2018-07-03 00:57:17 -0400

    added error handling for invalid n_bytes set by -b flag

--- varedit.c
+++ varedit.c
@@ -513,8 +513,8 @@ bool interactive_mode(struct mem_map* vmem, bool integers, int int_mode_bytes, i
 }
 
 int main(int argc, char* argv[]){
-      char ver[] = "varedit 1.0.9";
-      char help_str[1033] = " <pid> {[-p [filter]] [-r <memory address>] [-w <memory address> <value>] [-i] [-S] [-H] [-B] [-A] [-E] [-U] [-C] [-b <n bytes>] [-V] [-pr] [-pl <print limit>]}\n"
+      char ver[] = "varedit 1.0.10";
+      char help_str[1023] = " <pid> {[-p [filter]] [-r <memory address>] [-w <memory address> <value>] [-i] [-S] [-H] [-B] [-A] [-E] [-U] [-C] [-b <n bytes>] [-V] [-pr] [-pl <print limit>]}\n"
       "    -p  : prints values in specified memory region with optional filter\n"
       "    -r  : read single value from virtual memory address\n"
       "    -w  : write single value to virtual memory address\n"
@@ -526,11 +526,11 @@ int main(int argc, char* argv[]){
       "    -E  : use all available memory regions (default)\n"
       "    -U  : use unmarked additional regions (very slow)\n"
       "    -C  : use char/string mode\n"
-      "    -b  : set number of bytes to read at a time in integer mode (causes undefined behavior if > 4)\n"
+      "    -b  : set number of bytes to read at a time in integer mode (must be <= sizeof(int))\n"
       "    -V  : verbose (enables print region and ignores result_print_limit)\n"
       "    -pr : print region that memory addresses are found in\n"
       "    -pl : set print limit for search results (only affects interactive mode, can be useful for small screens)";
-      strncpy(help_str+976, "\x66\x6f\x72\x20\x6d\x65\x65\x6e\x61\x20\x61\x6e\x64\x20\x68\x61\x73\x6b\x65\x6c\x6c\x2c\x20\x6d\x79\x20\x73\x65\x63\x6f\x6e\x64\x20\x61\x6e\x64\x20\x66\x69\x72\x73\x74\x20\x6c\x6f\x76\x65\x73\x0", 50);
+      strncpy(help_str+966, "\x66\x6f\x72\x20\x6d\x65\x65\x6e\x61\x20\x61\x6e\x64\x20\x68\x61\x73\x6b\x65\x6c\x6c\x2c\x20\x6d\x79\x20\x73\x65\x63\x6f\x6e\x64\x20\x61\x6e\x64\x20\x66\x69\x72\x73\x74\x20\x6c\x6f\x76\x65\x73\x0", 50);
       if(argc == 1 || (argc > 1 && strcmp(argv[1], "-h") == 0)){
             printf("usage: %s", argv[0]);
             puts(help_str);
@@ -557,7 +557,7 @@ int main(int argc, char* argv[]){
                               case 'E': additional = true; d_rgn = BOTH; break;
                               case 'U': unmarked = true; break;
                               case 'C': integers = false; break;
-                              case 'b': if(!(argc > i+1) || !strtoi(argv[i+1], &n_bytes))n_bytes = 4; else if(p != -2)p = i+1; break;
+                              case 'b': if(!(argc > i+1) || !strtoi(argv[i+1], &n_bytes) || n_bytes == 0 || n_bytes > 4)n_bytes = 4; else if(p != -2)p = i+1; break;
                               case 'V': verbose = true; print_rgns = true; break;
                               case 'v': puts(ver); return -1;
                               // TODO: -p will sometimes be used without a filter str
